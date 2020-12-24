893060e5609b05902ea464e0a1972fe142a1b847
下田　佳紀 <y.s@platinum.local>
2020-12-24 17:49:22 +0900

    メール送信元/メール送信メソッド/宛先記述

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,2 +1,9 @@
 class AgendaMailer < ApplicationMailer
+  default from: 'admin@example.com'
+  layout 'mailer'
+
+  def destroy_mail(agenda)
+    @agenda = agenda
+    mail to: agenda.team.users.pluck(:email), subject: I18n.t("views.messages.agenda_destroy_notice")
+  end
 end
