b812e26314f3d49cc75bdfc8eaa1254d97f03cd8
下田　佳紀 <y.s@platinum.local>
2020-12-27 00:32:09 +0900

    日本語化適応

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -2,6 +2,6 @@ class TeamMailer < ApplicationMailer
   def authority_transfer_mail(old_owner, team)
     @old_owner = old_owner
     @team = team
-    mail to: "#{team.owner.email}", subject: "新リーダー就任のお知らせ"
+    mail to: "#{team.owner.email}", subject: I18n.t('views.messages.notice_new_leader')
   end
 end
